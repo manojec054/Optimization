bee73eb275e65dbb6d95403925fd71f9d1a58f00
Manoj Kumar Shivakumara Swamy (MS/ESW1-XC) <ShivakumaraSwamy.ManojKumar2@in.bosch.com>
2022-03-08 08:58:41 +0530

    tvm install updated

--- TVM/tvm_setup.sh
+++ TVM/tvm_setup.sh
@@ -1,3 +1,6 @@
+# https://github.com/manojec054/Optimization.git
+# sudo apt-get install llvm-10*
+
 cd ~
 git clone --recursive https://github.com/apache/tvm tvm
 cd tvm
@@ -7,8 +10,15 @@ git submodule update
 conda env create --file conda/build-environment.yaml
 conda activate tvm-build
 
-mkdir build
+mkdir -p ~/tvm/build
 cp ~/Optimization/TVM/config.cmake ~/tvm/build/
 cd ~/tvm/build
+
+sudo apt-get -y install llvm-10*
+
 cmake ..
 make -j9
+conda activate pytorch_p38
+export LD_LIBRARY_PATH=~/tvm/build:$LD_LIBRARY_PATH
+export TVM_HOME=~/tvm
+export PYTHONPATH=$TVM_HOME/python:${PYTHONPATH}
