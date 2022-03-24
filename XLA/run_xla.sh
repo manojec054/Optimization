b65a0a7320facc5f8b5c423c1c3c6b0e09b1888d
Manoj Kumar Shivakumara Swamy (MS/ESW1-XC) <ShivakumaraSwamy.ManojKumar2@in.bosch.com>
2022-03-24 22:17:48 +0530

    xla run scripts added

--- XLA/run_xla.sh
+++ XLA/run_xla.sh
@@ -1,19 +1,19 @@
 
-for model_name in resnet50 vgg16 ownmodel
+for model_name in resnet50 resnet101
     do
     echo "\n\n Using $model_name"
     #python xla_explore.py --only-train --jit-compile-flag --create-model $model_name
-    python xla_explore.py --only-train --create-model $model_name
+    python xla_explore.py --only-train --create-model $model_name --dir-path ~/Optimization/Dataset
 
     for batch in 1 4 8 16 32 64 128 256
         do 
         echo "\n\nXLA Enabled Runs, batch = $batch"
-        python xla_explore.py --jit-compile-flag --infe-batch $batch --create-model $model_name
+        python xla_explore.py --jit-compile-flag --infe-batch $batch --create-model $model_name --dir-path ~/Optimization/Dataset
         done
 
     for batch in 1 4 8 16 32 64 128 256
         do 
         echo "\n\nXLA Disabled Runs, batch = $batch"
-        python xla_explore.py --infe-batch $batch --create-model $model_name
+        python xla_explore.py --infe-batch $batch --create-model $model_name --dir-path ~/Optimization/Dataset
         done
 done
